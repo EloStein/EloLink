2b46229195adf6ef30b1741e66cc1991d821be0f
EloStein <steinole@yahoo.com>
2026-01-13 22:46:34 +0100

    Holy Moly Changes 2

---
+++ src/main/java/de/spring/elolink_spring/rest/dto/ChatDto.java
@@ -0,0 +1,119 @@
+package de.spring.elolink_spring.rest.dto;
+
+
+public class ChatDto {
+
+    private String signature;
+    private String sender;
+    private String receiver;
+    private String message;
+    private String aesKeyRec;
+    private String aesKeyOwn;
+    private String spec;
+    private String timestamp;
+
+    public ChatDto(){
+    }
+
+    public ChatDto(String signature, String sender, String receiver, String message, String aesKeyRec, String aesKeyOwn, String spec, String timestamp) {
+        this.signature = signature;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.message = message;
+        this.aesKeyRec = aesKeyRec;
+        this.aesKeyOwn = aesKeyOwn;
+        this.spec = spec;
+        this.timestamp = timestamp;
+    }
+
+    /*
+        public static ChatDto fromChat(Chat chat){
+            return new ChatDto("true",
+                    chat.getSender(),
+                    chat.getReceiver(),
+                    chat.getMessage(),
+                    chat.getAesKeyRec(),
+                    chat.getAesKeyOwn(),
+                    chat.getSpec(),
+                    chat.getTimestamp()
+            );
+        }
+    */
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getAesKeyRec() {
+        return aesKeyRec;
+    }
+
+    public void setAesKeyRec(String aesKeyRec) {
+        this.aesKeyRec = aesKeyRec;
+    }
+
+    public String getAesKeyOwn() {
+        return aesKeyOwn;
+    }
+
+    public void setAesKeyOwn(String aesKeyOwn) {
+        this.aesKeyOwn = aesKeyOwn;
+    }
+
+    public String getSpec() {
+        return spec;
+    }
+
+    public void setSpec(String spec) {
+        this.spec = spec;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "ChatDto{" +
+                "signature='" + signature + '\'' +
+                ", sender='" + sender + '\'' +
+                ", receiver='" + receiver + '\'' +
+                ", message='" + message + '\'' +
+                ", aesKeyRec='" + aesKeyRec + '\'' +
+                ", aesKeyOwn='" + aesKeyOwn + '\'' +
+                ", timestamp='" + timestamp + '\'' +
+                '}';
+    }
+}
